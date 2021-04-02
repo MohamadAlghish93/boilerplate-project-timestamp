bd152455a4a32237c4a226fbde93a03856c65cb2
Mhd Alghosh <49556550+MohamadAlghish93@users.noreply.github.com>
2021-04-02 10:31:20 +0400

    Update server.js

--- server.js
+++ server.js
@@ -25,6 +25,43 @@ app.get("/api/hello", function (req, res) {
 });
 
 
+app.get("/api/timestamp/:date?", function (req, res) {
+  
+  let date = req.params.date;
+  let toUTC = 0;
+  let toUnix = 0;
+  if (date != undefined) {
+
+    let tmp = new Date(date);
+    var num = isNaN(parseInt(date)) ? 0 : parseInt(date)
+    if ( tmp !== "Invalid Date" && !isNaN(new Date(tmp))) {
+      toUTC = tmp.toUTCString();
+      toUnix = tmp.getTime() / 1000;
+    } else if (num != 0) {
+      toUnix = num;
+      const unixTime = num;
+      tmp = new Date(unixTime);
+      toUTC = tmp.toUTCString();
+    } 
+    else {
+      return res.json({ error : "Invalid Date" });
+    }         
+  } else {
+
+    tmp = new Date();
+    toUTC = tmp.toUTCString();
+    toUnix = tmp.getTime() / 1000;
+    toUnix =  toUnix.toString().replace('.', '');
+
+  }
+
+  let obj = {
+      unix:toUnix, 
+      utc:toUTC
+    }
+    res.json(obj);
+  
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
